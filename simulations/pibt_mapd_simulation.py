2e393cd6801067854b33ef7269ddbfd8b77ce2c6
David Zeman <davca.zeman@gmail.com>
2025-12-06 14:14:32 +0100

    pibt cpp

---
+++ simulations/pibt_mapd_simulation.py
@@ -0,0 +1,14 @@
+from models.simulation import SimulationBase
+
+class PIBTMAPDSimulation(SimulationBase):
+    def step(self):
+
+        # Assign tasks and update goals
+        for agent in self.agents:
+            pass
+
+        # Plan using PIBT
+        for agent in self.agents:
+            pass
+
+        # Act agents
